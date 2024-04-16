2053391012ec0f446c4dac798a4a46957cda6071
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-04-16 08:41:35 +0600

    automatically filling gaps in the records

--- src/modules/weather/WeatherCompareChart.tsx
+++ src/modules/weather/WeatherCompareChart.tsx
@@ -5,6 +5,7 @@ import dayjs from "dayjs";
 import { colors, windowSize } from "@/constants";
 
 type ComparableData = {
+    date: Date;
     actualTemp?: number;
     forecastTemp?: number;
     actualHumidity?: number;
@@ -18,26 +19,7 @@ const WeatherCompareChart: React.FC = () => {
     const [height, setHeight] = useState(400);
 
     useEffect(() => {
-        const cd: Record<string, ComparableData> = {};
-        for (let i = 0; i < weatherInfo.length; ++i) {
-            const w = weatherInfo[i];
-            const recordKey = dayjs(w.date).format("DD-MM-YYYY-HH");
-            if (w.forecast) {
-                if (cd[recordKey]) {
-                    cd[recordKey].forecastTemp = w.temperature_2m;
-                    cd[recordKey].forecastHumidity = w.relativehumidity_2m;
-                } else {
-                    cd[recordKey] = { forecastTemp: w.temperature_2m };
-                }
-            } else {
-                if (cd[recordKey]) {
-                    cd[recordKey].actualTemp = w.temperature_2m;
-                } else {
-                    cd[recordKey] = { actualTemp: w.temperature_2m };
-                }
-            }
-        }
-        setData(Object.values(cd));
+        prepareChartData();
     }, [weatherInfo]);
 
     useEffect(() => {
@@ -47,6 +29,72 @@ const WeatherCompareChart: React.FC = () => {
             window.removeEventListener("resize", calcFrameSize);
         };
     }, []);
+
+    function prepareChartData() {
+        const cd: Record<string, ComparableData> = {};
+        for (let i = 0; i < weatherInfo.length; ++i) {
+            const w = weatherInfo[i];
+            const recordKey = dayjs(w.date).format("DD-MM-YYYY-HH");
+            if (w.forecast) {
+                if (cd[recordKey]) {
+                    cd[recordKey].forecastTemp = w.temperature_2m;
+                    cd[recordKey].forecastHumidity = w.relativehumidity_2m;
+                } else {
+                    cd[recordKey] = {
+                        date: w.date,
+                        forecastTemp: w.temperature_2m,
+                    };
+                }
+            } else {
+                if (cd[recordKey]) {
+                    cd[recordKey].actualTemp = w.temperature_2m;
+                } else {
+                    cd[recordKey] = {
+                        date: w.date,
+                        actualTemp: w.temperature_2m,
+                    };
+                }
+            }
+        }
+        const sortedData = Object.values(cd).sort((a, b) => {
+            if (dayjs(a.date).isBefore(b.date, "h")) return -1;
+            return 1;
+        });
+        for (let i = 0; i < sortedData.length; ++i) {
+            const current = sortedData[i];
+            if (current.forecastTemp !== undefined && current.actualTemp !== undefined) {
+                continue;
+            }
+            let alt = null;
+            for (let j = i; j > -1; --j) {
+                const p = sortedData[j];
+                if ((!current.forecastTemp && !p.forecastTemp) || (!current.actualTemp && !p.actualTemp)) {
+                    continue;
+                }
+                alt = p;
+                break;
+            }
+            if (!alt) {
+                for (let j = i; j < sortedData.length; ++j) {
+                    const n = sortedData[j];
+                    if ((!current.forecastTemp && !n.forecastTemp) || (!current.actualTemp && !n.actualTemp)) {
+                        continue;
+                    }
+                    alt = n;
+                    break;
+                }
+            }
+            if (alt) {
+                if (!current.forecastTemp) {
+                    current.forecastTemp = alt.forecastTemp;
+                }
+                if (!current.actualTemp) {
+                    current.actualTemp = alt.actualTemp;
+                }
+            }
+        }
+        setData(sortedData);
+    }
 
     function calcFrameSize() {
         setHeight(window.innerHeight * 0.45);
